447c66c00b3a537f17ea3ed31cb6f8d93322c4ba
David Moates <david.m.moates@gmail.com>
2019-10-11 21:04:51 -0500

    Started Sprint and got API and Displayed items

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,43 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+
+import styled from 'styled-components';
+
+
+import CharacterCard from "./CharacterCard.js";
+
+
+const DivFlex = styled.div`
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  margin: 10px;
+  flex-wrap: wrap;
+`;
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [charRM, setCharRM] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    axios.get("https://rickandmortyapi.com/api/character/")
+    .then(res => {
+      console.log(res.data.results);
+      setCharRM(res.data.results);
+
+    })
+    .catch(error => {
+      console.log("Error:", error);
+    })
+  },[])
 
   return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
+  <DivFlex>
+    {
+      charRM.map((char, index) => <CharacterCard key = {index} cObject = {char} />)
+    }
+  </DivFlex>
   );
 }
