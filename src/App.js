496188dd49ae0a19cc08f956d319541386301963
David Moates <david.m.moates@gmail.com>
2019-10-11 21:29:46 -0500

    done with route now need to get the search form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
-import Header from "./components/Header";
+import { Route } from 'react-router-dom';
+
+
+import Header1 from "./components/Header";
+import WelcomePage from "./components/WelcomePage";
 import CharacterList from "./components/CharacterList";
 
 
 export default function App() {
   return (
     <main>
-      <Header />
-      <CharacterList />
+      <Header1 />
+        <Route exact path="/" component={WelcomePage} />
+        <Route path="/character-list" component={CharacterList} />
     </main>
   );
 }
