ea1cbe7ce2e019359ff8acab2ce03135d301bb63
David Moates <david.m.moates@gmail.com>
2019-12-13 20:57:36 -0600

    fixed welcome page

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,11 +1,20 @@
 import React from "react";
 
+import styled from "styled-components";
+
+const Img = styled.img`
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+`;
+
 export default function WelcomePage() {
   return (
     <section className="welcome-page">
       <header>
         <h1>Welcome to the ultimate fan site!</h1>
-        <img
+        <Img
           className="main-img"
           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
           alt="rick"
